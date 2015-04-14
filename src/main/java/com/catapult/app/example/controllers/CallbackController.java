5c39692ab533bdb4c1e0d085cdb06eb008f18230
Leonardo Vivo <lvivo@daitangroup.com>
2015-04-14 14:10:16 -0300

    Adding callbacks log

--- src/main/java/com/catapult/app/example/controllers/CallbackController.java
+++ src/main/java/com/catapult/app/example/controllers/CallbackController.java
@@ -2,6 +2,7 @@ package com.catapult.app.example.controllers;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,8 @@ public class CallbackController {
     @Autowired
     private CallbackServices callbackServices;
 
+    private final static Logger LOG = Logger.getLogger(CallbackController.class);
+    
     /**
      * Resource to receive the user callback requests.
      * @param event the callback event with the data.
@@ -26,7 +29,7 @@ public class CallbackController {
     @RequestMapping(method = RequestMethod.POST, value = "/{userName}/callback", headers = "Content-Type=application/json")
     public void receiveCallback(@RequestBody final String event,
                                 @PathVariable("userName") final String userName, final HttpServletRequest request) {
+        LOG.info(String.format("Received callback for event: %s. UserName:  %s", event, userName));
         callbackServices.handleCallback(event, userName, URLUtil.getAppBaseUrl(request));
     }
-
 }
