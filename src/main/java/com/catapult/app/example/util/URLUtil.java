2b67aaaa157e7e7041ca900f46d919b1d9f244d2
Leonardo Vivo <lvivo@daitangroup.com>
2015-04-14 12:54:59 -0300

    CallbackURL fix

--- src/main/java/com/catapult/app/example/util/URLUtil.java
+++ src/main/java/com/catapult/app/example/util/URLUtil.java
@@ -4,14 +4,14 @@ import javax.servlet.http.HttpServletRequest;
 
 public class URLUtil {
 
-    private static final String APP_BASE_URL = "%s://%s:%d/%s";
+    private static final String APP_BASE_URL = "%s://%s:%d%s";
 
     /**
      * Configure the operations callback URL.
      * @return the callbacks URL.
      */
     public static String getCallbacksBaseUrl(final String appBaseUrl, final String userName) {
-        return String.format("%s%s/callback", appBaseUrl, userName);
+        return String.format("%s/%s/callback", appBaseUrl, userName);
     }
 
     /**
