6aebc02f7376f1532cda83da728595f65c6758b7
Leonardo Vivo <lvivo@daitangroup.com>
2015-04-14 09:04:44 -0300

    Property files fix

--- src/main/java/com/catapult/app/example/configuration/UserConfiguration.java
+++ src/main/java/com/catapult/app/example/configuration/UserConfiguration.java
@@ -2,6 +2,7 @@ package com.catapult.app.example.configuration;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -10,26 +11,30 @@ import com.bandwidth.sdk.BandwidthClient;
 @Component
 public class UserConfiguration {
 
-    @Value(value = "${sandbox.user.id}")
+    @Value(value = "${sandbox.user.id:}")
     private String userId;
     
-    @Value(value = "${sandbox.api.key}")
+    @Value(value = "${sandbox.api.key:}")
     private String userApiKey;
     
-    @Value(value = "${sandbox.api.secret}")
+    @Value(value = "${sandbox.api.secret:}")
     private String userApiSecret;
     
-    @Value(value = "${sandbox.api.url}")
+    @Value(value = "${sandbox.api.url:}")
     private String apiUrl;
     
-    @Value(value = "${sandbox.api.version}")
+    @Value(value = "${sandbox.api.version:}")
     private String apiVersion;
    
     @PostConstruct
     public void userConfiguration() {
         BandwidthClient bandwidthClient = BandwidthClient.getInstance();
-        bandwidthClient.setCredentials(userId, userApiKey, userApiSecret);
-        bandwidthClient.setEndpointandVersion(apiUrl, apiVersion);
+        if(!StringUtils.isBlank(userId) && !StringUtils.isBlank(userApiKey) && !StringUtils.isBlank(userApiSecret)) {
+            bandwidthClient.setCredentials(userId, userApiKey, userApiSecret);
+        }
+        if(!StringUtils.isBlank(apiUrl) && !StringUtils.isBlank(apiVersion)) {
+            bandwidthClient.setEndpointandVersion(apiUrl, apiVersion);
+        }
     }
 
     /**
